5d1cdfbd4fb78e0884abc833a098e900de65bb26
Arzeusy <jkob1994@gmai.com>
2023-10-03 12:53:01 -0600

    fix (main) se agrego las opciones por default de firebase para la autenticacion e inicializacion

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   // keep splash screen until initializationhas completed!
 
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
